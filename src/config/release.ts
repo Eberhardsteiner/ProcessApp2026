681739f95ecbbad46cc563bc3903f4f134a6d540
Eberhardsteiner <Eberhard.steiner@uvm-institut.de>
2026-04-03 09:23:20 +0200

    2

--- src/config/release.ts
+++
@@ -1,3 +0,0 @@
-export const APP_SEMVER = '0.20.0';
-export const APP_VERSION_LABEL = 'v0.20';
-export const APP_RELEASE_TITLE = 'Phase 20 · Mehr Goldfälle und breitere Fachpakete';
--- src/storage/index.ts
+++
@@ -1,4 +0,0 @@
-export * from './indexedDb';
-export * from './repositories/projectsRepo';
-export * from './repositories/processesRepo';
-export * from './repositories/versionsRepo';
