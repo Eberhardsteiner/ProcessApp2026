2034706dc27e3a2a7c0636edb8dd5442cfd94d0d
Eberhardsteiner <Eberhard.steiner@uvm-institut.de>
2026-04-07 08:11:52 +0200

    1

--- src/config/release.ts
+++
@@ -1,3 +0,0 @@
-export const APP_SEMVER = '0.40.1';
-export const APP_VERSION_LABEL = 'v0.40.1';
-export const APP_RELEASE_TITLE = 'Qualitätscheck-Export für manuelle Referenzläufe';
--- src/domain/index.ts
+++
@@ -1,3 +0,0 @@
-export * from './process';
-export * from './capture';
-export * from './bpmn';
